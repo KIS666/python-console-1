8362c94125225a3ea30096df3778d8abaf24cbe5
KIS666 <120309902+KIS666@users.noreply.github.com>
2022-12-11 16:08:06 +0300

    Delete pair.py

--- pair/pair.py
+++
@@ -1,9 +0,0 @@
-boys = ['Peter', 'Alex', 'John', 'Arthur', 'Richard']
-girls = ['Kate', 'Liza', 'Kira', 'Emma', 'Trisha']
-if len(boys)!=len(girls):
-  print('Кто-то может остаться без пары:(')
-  exit()
-boys.sort()
-girls.sort()
-for name_boy, name_girl in zip(boys, girls):
-  print(name_boy,'и', name_girl)
